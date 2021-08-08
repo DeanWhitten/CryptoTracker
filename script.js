d1589341d9e078674ed2fb824f38a186df7d8f99
dean <dean.f.whitten@gmail.com>
2021-08-08 19:59:52 -0400

    added percentage change attribute, styling based on percentage change
    new feat that changes background color based on the percentage of change over the past hour.

--- script.js
+++ script.js
@@ -11,23 +11,25 @@ function coinCard (cRank, cName, cPrice, cId){
 };
 
 //adds coin to cryptoCoin array
-function addData(dataRank, dataName, dataPrice, id){
+function addData(dataRank, dataName, dataPrice, dataPercentChange){
     coinCard.cRank = dataRank;
     coinCard.cName = dataName;
     coinCard.cPrice = dataPrice;
     coinCard.cId = cryptoCoin.length;
+    coinCard.cPercent = dataPercentChange;
 
-    cryptoCoin.push([coinCard.cRank, coinCard.cName, coinCard.cPrice, cryptoCoin.id]);
+    cryptoCoin.push([coinCard.cRank, coinCard.cName, coinCard.cPrice, cryptoCoin.id, dataPercentChange]);
 
-    buildCard(coinCard.cRank, coinCard.cName, coinCard.cPrice, cryptoCoin.id);
+    buildCard(coinCard.cRank, coinCard.cName, coinCard.cPrice, cryptoCoin.id, dataPercentChange);
 }
 
 
 console.log(cryptoCoin); //test line
 
-function buildCard(bRank, bName, bPrice, bId){
+function buildCard(bRank, bName, bPrice, bId, bPercent){
     let coinObject = document.createElement("div");
     coinObject.setAttribute('id', 'card' );
+
 
     const newRank = document.createElement('h5');
     const newName = document.createElement('h4');
@@ -39,7 +41,7 @@ function buildCard(bRank, bName, bPrice, bId){
 
     newRank.setAttribute('id', 'rank');
     newName.setAttribute('id', 'name');
-    newPrice.setAttribute('id', 'name');
+    newPrice.setAttribute('id', 'price');
 
 
     newRank.appendChild(insertRank);
@@ -52,6 +54,18 @@ function buildCard(bRank, bName, bPrice, bId){
 
     const container =document.getElementById("container");
     container.appendChild(coinObject);
+
+    //parses string to float
+    let percent = parseFloat(bPercent);
+    console.log(percent);
+
+
+//changes background color based on negative or positive precentage changes hourly
+    if (percent > - 0.00 ){
+        coinObject.style.backgroundColor = "#57B931"; //green
+    } else{
+        coinObject.style.backgroundColor = "#D31512"; //red
+    }
 }
 
 
@@ -70,8 +84,8 @@ function loadData(){
         data.forEach((coin) => {
             if(coin.rank <=20 ){
                 // Log each coin
-                console.log("Name: ", coin.name, "  Price:", coin.price_usd, "  Rank:", coin.rank);
-                addData(coin.rank, coin.name, coin.price_usd);
+                console.log("Name: ", coin.name, "  Price:", coin.price_usd, "  Rank:", coin.rank , coin.percent_change_1h);//added coin % change
+                addData(coin.rank, coin.name, coin.price_usd, coin.percent_change_1h);
             }
 
         })
